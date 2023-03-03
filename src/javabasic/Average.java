479a26878871d34e0dd05ad45ef4a6b0d112fb74
Maria-Ziborova <124693876+Maria-Ziborova@users.noreply.github.com>
2023-03-03 16:38:34 +0700

    Update Averege

--- src/javabasic/Average.java
+++ src/javabasic/Average.java
@@ -1,26 +1,22 @@
 package javabasic;
 
 public class Average {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+        int i = 0;
+        int j = 0;
         int a = 3;
-        int b = 5;
+        int b = 17;
         double sum = 0;
         double sum1 = 0;
-        int i = a;
-        int j = a;
-        for (; i < b; i++) {
-            sum = sum + i;
-            i = i + 1;
+        for(;a - b <= 0;)
+        {
+            i++;
+            if (a % 2 == 0) { sum1 += a; j += 1; }
+            sum += a;
+            a += 1;
         }
-        for (; j < b; j++) {
-            if (j % 2 == 0) {
-                sum1 = sum1 + j;
-                j = j + 1;
-            }
-        }
-        double average = (double) sum / i;
-        double average1 = (double) sum1 / j;
-        System.out.println("Среднее арифметическое чисел = " + average);
-        System.out.println("Среднее арифметическое четных чисел = " + average1);
+        System.out.println("Среднее арифметическое чисел = " + sum / i);
+        System.out.println("Среднее арифметическое четных чисел = " + sum1 / j);
     }
 }
