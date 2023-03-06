13e3a9cf5be88730581220c955bc77a11466c3ff
geolog5 <@Maria-Ziborova>
2023-03-06 15:36:05 +0700

    Add file Avg.java

---
+++ src/javabasic/Avg.java
@@ -0,0 +1,35 @@
+package javabasic;
+
+import java.util.Scanner;
+
+public class Avg {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите начало диапазона: ");
+        int a = scanner.nextInt();
+        System.out.print("Введите конец диапазона: ");
+        int b = scanner.nextInt();
+        int i = a;
+        int j = a;
+        int count = 0;
+        int countEwen = 0;
+        double sum = 0;
+        double sumEven = 0;
+        while (i <= b)
+        {
+            sum += i;
+            count += 1;
+            i++;
+        }
+        while (j <= b)
+        {
+            if (j % 2 == 0) {
+                sumEven += j;
+                countEwen += 1;
+            }
+            ++j;
+        }
+        System.out.println("Среднее арифметическое = " + sum / count);
+        System.out.println("Среднее арифметическое четных = " + sumEven / countEwen);
+    }
+}
