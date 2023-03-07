8882b8aa7690771448cdc7b4ef14f52cb07cd122
Maria-Ziborova <124693876+Maria-Ziborova@users.noreply.github.com>
2023-03-07 14:25:27 +0700

    Update Average.java

--- src/javabasic/Average.java
+++ src/javabasic/Average.java
@@ -6,30 +6,24 @@ public class Average {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Введите начало диапазона: ");
-        int a = scanner.nextInt();
+        int startRange = scanner.nextInt();
         System.out.print("Введите конец диапазона: ");
-        int b = scanner.nextInt();
-        int i = a;
-        int j = a;
+        int endRange = scanner.nextInt();
+        int i = startRange;
         int count = 0;
         int countEwen = 0;
-        double sum = 0;
-        double sumEven = 0;
-        while (i <= b)
-        {
+        int sum = 0;
+        int sumEven = 0;
+        while (i <= endRange){
+            if (i % 2 == 0) {
+                sumEven += i;
+                countEven += 1;
+            }
             sum += i;
             count += 1;
             i++;
         }
-        while (j <= b)
-        {
-            if (j % 2 == 0) {
-                sumEven += j;
-                countEwen += 1;
-            }
-            ++j;
-        }
-        System.out.println("Среднее арифметическое = " + sum / count);
-        System.out.println("Среднее арифметическое четных = " + sumEven / countEwen);
+        System.out.println("Среднее арифметическое = " + (double) sum / count);
+        System.out.println("Среднее арифметическое четных = " + (double) sumEven / countEven);
     }
 }
